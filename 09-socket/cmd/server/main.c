3fba529a8061c061c5fc1d8fd584c361179ac48f
Kauan da Rosa Paulino <55677667+KRPLAB@users.noreply.github.com>
2026-03-31 11:54:46 -0300

    Refactor server main function for raw socket

--- 09-socket/cmd/server/main.c
+++ 09-socket/cmd/server/main.c
@@ -1,37 +1,24 @@
-#include "../../include/common.h"
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../../include/common.h"
 
-int main(void) {
-    log_msg("Redis Init: Starting Server on port 6379...");
+int main() {
+    // Porta padrão para o servidor HTTP. Se for raw socket pode comentar essa linha
+    // int port = 8080;
+    // printf("[MAIN] Iniciando o servidor na porta %d...\n", port);
 
-    int server_fd = create_server_socket(6379);
-    if (server_fd < 0) {
-        return 1;
-    }
-    
-    log_msg("Waiting for connections...");
+    printf("[MAIN] Iniciando o servidor raw socket...\n");
 
-    while (1) {
-        struct sockaddr_in client_addr;
-        socklen_t addr_len = sizeof(client_addr);
-        
-        // accept bloqueia até alguém conectar
-        int conn_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
-        
-        if (conn_fd < 0) {
-            log_msg("Erro no accept (ignorando)");
-            continue; // Em servidor real, não paramos por um erro de conexão
-        }
+    // int server_fd = net_server_start(port);
+    int server_fd = net_raw_socket_start();
 
-        log_msg("Client connected!");
-        
-        // Por enquanto, apenas fechamos para evitar vazamento de fd
-        close(conn_fd); 
+    if (server_fd == -1) {
+        exit(EXIT_FAILURE);
     }
 
     close(server_fd);
+    printf("[MAIN] Servidor encerrado.\n");
+
     return 0;
 }
